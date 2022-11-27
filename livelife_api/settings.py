3e9b2ec36db29fe0d1d8becc2894947b48242ae2
Attstar2021 <attiyarasheed923@yahoo.co.uk>
2022-11-27 20:30:39 +0000

    debug deployment setting

--- livelife_api/settings.py
+++ livelife_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'livelife.api.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'live-life-api.herokuapp.com']
 
 
 # Application definition
